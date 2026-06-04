593fd0a871579d985f85aa6674ab4d1298dea550
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-04 14:34:13 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "landingzone" {
   name     = "rg-secure-landing-zone"
   location = "West Europe"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-secure-lz"
+  location            = azurerm_resource_group.landingzone.location
+  resource_group_name = azurerm_resource_group.landingzone.name
+  address_space       = ["10.0.0.0/16"]
+}
