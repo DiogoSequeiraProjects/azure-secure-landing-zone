61941d69508bd8a755370ef19255860eff8ac335
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-04 14:31:01 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "landingzone" {
+  name     = "rg-secure-landing-zone"
+  location = "West Europe"
+}
