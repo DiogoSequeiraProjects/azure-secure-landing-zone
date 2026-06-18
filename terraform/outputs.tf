06bb827093c1bf2e6696117ba1760e619011564e
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-18 12:07:21 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,15 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.landingzone.name
+}
 
+output "virtual_network_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "private_subnet_id" {
+  value = azurerm_subnet.private.id
+}
+
+output "network_security_group_name" {
+  value = azurerm_network_security_group.private_nsg.name
+}
