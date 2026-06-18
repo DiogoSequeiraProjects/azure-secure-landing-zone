7c0b64819b9e3b4b4805cf0465814b3a47d5b34a
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-18 12:07:09 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "location" {
+  description = "Azure region for the landing zone resources."
+  type        = string
+  default     = "West Europe"
+}
 
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  type        = string
+  default     = "rg-secure-landing-zone"
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network."
+  type        = string
+  default     = "vnet-secure-lz"
+}
