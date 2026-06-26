3022b3dcef20de692992a4187ad825d0608cd7e2
DiogoSequeiraProjects <diogo.sequeira2004@gmail.com>
2026-06-26 12:11:16 +0100

    Associate NSG with management subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,10 @@ resource "azurerm_subnet" "management" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_subnet_network_security_group_association" "management" {
+  subnet_id                 = azurerm_subnet.management.id
+  network_security_group_id = azurerm_network_security_group.private_nsg.id
+}
 
 resource "azurerm_subnet" "private" {
   name                 = "subnet-private"
